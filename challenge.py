7b0199a976254cd13bf93928e9dd4e24350430ca
Jorge Diaz <jorgdiaz.lara@gmail.com>
2020-05-11 14:33:31 -0500

    Scope resuelto

--- challenge.py
+++ challenge.py
@@ -2,8 +2,10 @@ def make_division_by(n):
     """This closure returns a function that returns the division
        of an x number by n 
     """
-    # You have to code here!
-    pass
+    def repeater (num):
+        res = num / n
+        return res 
+    return repeater
 
 
 def run():
@@ -16,13 +18,31 @@ def run():
     division_by_18 = make_division_by(18)
     print(division_by_18(54))  # The expected output is 3
 
+    unittest.main()
+
 
 if __name__ == '__main__':
     import unittest
 
     class ClosureSuite(unittest.TestCase):
-        def test_closure_make_division_by(self):
-            # Make the closure test here
-            pass
 
+        def setUp (self):
+            self.value_LCM = 90
+
+        def test_closure_make_division_by(self):
+            division_by_3 = make_division_by(3)
+            self.assertEqual(30, division_by_3(self.value_LCM))
+            
+            division_by_5 = make_division_by(5)
+            self.assertEqual(18, division_by_5(self.value_LCM))
+
+
+            division_by_18 = make_division_by(18)
+            self.assertEqual(5, division_by_18(self.value_LCM))
+
+        def tearDown(self):
+            del(self.value_LCM)
+
+    
     run()
+
